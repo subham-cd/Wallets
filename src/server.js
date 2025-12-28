1ceb0f3ee7d280079879c0818ca2c6138f082281
Subham Singh <>
2025-12-28 19:12:29 +0530

    Implement cron job

--- src/server.js
+++ src/server.js
@@ -3,10 +3,13 @@ import dotenv from "dotenv";
 import { sql } from "./config/db.js";
 import ratelimiter from "./middleware/rateLimiter.js";
 import transactionRoute from "./routes/transaction.route.js";
+import job from "./config/cron.js";
 
 dotenv.config();
 
 const app = express();
+
+if(process.env.NODE_ENV === "production")job.start();
 
 app.use(ratelimiter);
 app.use(express.json());
@@ -36,7 +39,13 @@ app.get("/", (req, res) => {
   res.send("its working");
 });
 
+app.get("api/health",(req,res)=>{
+  res.status(200).json({status:"ok"})
+})
+
 app.use("/api/transactions", transactionRoute);
+
+
 
 initDB().then(() => {
   app.listen(PORT, () => {
